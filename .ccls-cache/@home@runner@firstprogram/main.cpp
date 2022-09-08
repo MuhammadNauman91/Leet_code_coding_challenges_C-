e83580a22dc00b384212ab2c10242d5dfd5760ad
Naumannasir90 <>
2022-09-08 06:08:03 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@firstprogram/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+ // int n = 10;
+  int sum = 0;
+  for (int i= 0; i<4 ; i++)
+    {
+     for (int j= 0; j<4 ; j++)
+       {
+         std::cout<< '*' ;
+       }
+      std::cout << endl;
+      }
+  
+  }
